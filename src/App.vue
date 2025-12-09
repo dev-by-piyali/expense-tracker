8b7459cedc64e694bf2957a7bc1ec610a174ce63
Piyali Nag <piyalinag99@gmail.com>
2025-12-09 15:49:03 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Welcome from "./components/Welcome.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Expense Tracker</h1>
+    <Welcome />
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped lang="scss"></style>
