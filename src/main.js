1a775911651664f94f1ba18d2d6cb7ee698e83df
Piyali Nag <piyalinag99@gmail.com>
2025-12-12 14:15:08 +0000

    use Vuetify and mdi-icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Vuetify
+import "vuetify/styles"; // Import Vuetify styles
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css"; // Import MDI font
+import "./assets/main.scss";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  // Optional: Add global configuration here (e.g., themes, icons)
+});
+
+createApp(App).use(vuetify).mount("#app");
